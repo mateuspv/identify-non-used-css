ddacfe99e8dc84602fc80c57c1054bff64291e3e
mateuspv <mateuspatzlaff@gmail.com>
2017-12-10 01:31:09 -0200

    improve dom test

--- tests/DOM.js
+++ tests/DOM.js
@@ -1,17 +1,27 @@
 const test = require('tape')
 const DOM = require('../lib/DOM').default;
 
-test('timing test', function (t) {
-    const html = new DOM(`
-      <!DOCTYPE html>
-        <html lang="en">
-          <head>
-          </head>
-        </html>
-    `);
+const template = `
+  <!DOCTYPE html>
+    <html lang="en">
+      <head>
+      </head>
+    </html>
+`;
 
-    t.plan(2);
+
+test('found query css test', function (t) {
+    const html = new DOM(template);
+
+    t.plan(1);
 
     t.equal(html.query('head').length, 1);
+});
+
+test('not found query css test', function (t) {
+    const html = new DOM(template);
+
+    t.plan(1);
+
     t.equal(html.query('.some_selector').length, 0);
 });
